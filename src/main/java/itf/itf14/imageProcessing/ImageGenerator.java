3625dcbb276681e34df2124303a788a119dab75d
BelitskyiAlexandr <vilisapett@gmail.com>
2024-10-07 20:37:30 +0300

    image generation fixes

--- src/main/java/itf/itf14/imageProcessing/ImageGenerator.java
+++ src/main/java/itf/itf14/imageProcessing/ImageGenerator.java
@@ -1,57 +1,87 @@
 package itf.itf14.imageProcessing;
 
-import org.springframework.stereotype.Component;
-
-import java.awt.Graphics2D;
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ImageGenerator {
-
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 120;
+    private static final int IMAGE_WIDTH = 300;
+    private static final int IMAGE_HEIGHT = 140;
+    private static final int BARCODE_HEIGHT = 100;
     private static final int BAR_WIDTH = 2;
+    private static final int THICK_BAR_WIDTH = BAR_WIDTH * 2;
+    private static final int BORDER_THICKNESS = THICK_BAR_WIDTH * 2;
+    private static final String[] PATTERNS = {
+            "00110", "10001", "01001", "11000", "00101",
+            "10100", "01100", "00011", "10010", "01010"
+    };
+    private static final String START_PATTERN = "00";
+    private static final String STOP_PATTERN = "10";
+    private static final String INVERTED_STOP_PATTERN = "00";
     private static final String FONT = "Arial";
+    private static final int FONT_STYLE = Font.PLAIN;
+    private static final int FONT_SIZE = 16;
+    private static final int LABEL_XCOOR = IMAGE_WIDTH / 24 * 7;
+    private static final int LABEL_YCOOR = (IMAGE_HEIGHT + BARCODE_HEIGHT) / 2 + 13;
+
+    private int x = IMAGE_WIDTH / 84 * 15;
+    private int y = BORDER_THICKNESS;
 
     public BufferedImage generateBarcodeImage(String barcode) {
-        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
-
-        // Фон білий
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-
-        // Малюємо штрихкод (чорні смуги)
+        g.fillRect(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
         g.setColor(Color.BLACK);
-        int x = 10;
+        g.setStroke(new BasicStroke(BORDER_THICKNESS));
+        g.drawRect(BORDER_THICKNESS / 2,BORDER_THICKNESS / 2,
+                IMAGE_WIDTH - BORDER_THICKNESS, BARCODE_HEIGHT + BORDER_THICKNESS);
+
+        StringBuilder oddPositionNumbers = new StringBuilder();
+        StringBuilder evenPositionNumbers = new StringBuilder();
         for (int i = 0; i < barcode.length(); i++) {
-            int digit = Character.getNumericValue(barcode.charAt(i));
-            String binary = getITF14BinaryRepresentation(digit);
-            for (char bit : binary.toCharArray()) {
-                if (bit == '1') {
-                    g.fillRect(x, 10, BAR_WIDTH, 100);
-                }
-                x += BAR_WIDTH;
-            }
+            (i % 2 == 1 ? oddPositionNumbers : evenPositionNumbers)
+                    .append(barcode.toCharArray()[i]);
         }
 
-        // Додаємо текст під штрихкодом
+        String evenPositionPatternedNumbers = START_PATTERN
+                + convertNumericStringToPatternString(evenPositionNumbers.toString())
+                + STOP_PATTERN;
+        String oddPositionPatternedNumbers = START_PATTERN
+                + convertNumericStringToPatternString(oddPositionNumbers.toString())
+                + INVERTED_STOP_PATTERN;
+
+        for (int i = 0; i < evenPositionPatternedNumbers.length(); i++) {
+            g.setColor(Color.BLACK);
+            x = drawBar(g, evenPositionPatternedNumbers.toCharArray()[i], x);
+            g.setColor(Color.WHITE);
+            x = drawBar(g, oddPositionPatternedNumbers.toCharArray()[i], x);
+        }
+
         g.setColor(Color.BLACK);
-        g.setFont(new Font(FONT, Font.PLAIN, 16));
-        g.drawString(barcode, 10, 135);
+        g.setFont(new Font(FONT, FONT_STYLE, FONT_SIZE));
+        g.drawString(barcode, LABEL_XCOOR, LABEL_YCOOR);
 
         g.dispose();
         return image;
     }
+    private String convertNumericStringToPatternString(String numericString) {
+        StringBuilder patternString = new StringBuilder();
+        for(char number : numericString.toCharArray()) {
+            patternString.append(PATTERNS[Character.getNumericValue(number)]);
+        }
+        return patternString.toString();
+    }
 
-    private String getITF14BinaryRepresentation(int digit) {
-        // Кожна цифра має відповідати певному двійковому коду за таблицею
-        String[] patterns = {
-                "00110", "10001", "01001", "11000", "00101",
-                "10100", "01100", "00011", "10010", "01010"
-        };
-        return patterns[digit];
+    private int drawBar(Graphics2D g, char bit, int x) {
+            if (bit == '1') {
+                g.fillRect(x, y, THICK_BAR_WIDTH, BARCODE_HEIGHT);
+                x += THICK_BAR_WIDTH;
+            } else {
+                g.fillRect(x, y, BAR_WIDTH, BARCODE_HEIGHT);
+                x += BAR_WIDTH;
+            }
+        return x;
     }
 }
