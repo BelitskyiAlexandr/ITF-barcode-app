09a7558b59c22e86a0bffe86bd2ec154bee7d4dc
BelitskyiAlexandr <vilisapett@gmail.com>
2024-10-16 21:27:35 +0300

    decoder done

--- src/main/java/itf/itffourteen/imageprocessing/BarcodeDecoder.java
+++ src/main/java/itf/itffourteen/imageprocessing/BarcodeDecoder.java
@@ -1,91 +1,89 @@
 package itf.itffourteen.imageprocessing;
 
 import java.awt.image.BufferedImage;
-import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BarcodeDecoder {
-
     private static final String[] PATTERNS = {
             "00110", "10001", "01001", "11000", "00101",
             "10100", "01100", "00011", "10010", "01010"
     };
 
-    public String decodeBarcode(BufferedImage barcodeImage) {
-        int width = barcodeImage.getWidth();
-        int height = barcodeImage.getHeight();
+    public String decodeBarcode(BufferedImage image) {
+        int width = image.getWidth();
 
-        int xCoordinate = findStartingPoint(barcodeImage);
-        int yCoordinate = height / 3;
+        int xCoordinate = ImageProperties.IMAGE_WIDTH / 84 * 15;
+        int yCoordinate = ImageProperties.BORDER_THICKNESS;
 
-        StringBuilder decodedBarcode = new StringBuilder();
+        List<Integer> barsAndSpaces = new ArrayList<>();
 
-        while (xCoordinate < width) {
-            String blackPattern = readPattern(barcodeImage, xCoordinate, yCoordinate, Color.BLACK);
-            String whitePattern = readPattern(barcodeImage, xCoordinate + blackPattern.length(), yCoordinate, Color.WHITE);
+        int currentBarWidth = 0;
+        boolean isBlack = isBlackColumn(image, xCoordinate, yCoordinate);
 
-            if (blackPattern.isEmpty() || whitePattern.isEmpty()) {
-                break;
+        for (int x = xCoordinate; x < width - xCoordinate; x++) {
+            boolean currentColumnIsBlack = isBlackColumn(image, x, yCoordinate);
+            if (currentColumnIsBlack == isBlack) {
+                currentBarWidth++;
+            } else {
+                barsAndSpaces.add(currentBarWidth);
+                isBlack = currentColumnIsBlack;
+                currentBarWidth = 1;
             }
-
-            String fullPattern = blackPattern + whitePattern;
-            String decodedDigit = decodePatternToDigit(fullPattern);
-
-            if (decodedDigit != null) {
-                decodedBarcode.append(decodedDigit);
-            }
-
-            xCoordinate += blackPattern.length() + whitePattern.length();
         }
 
-        return decodedBarcode.toString();
+        StringBuilder binarySequence = new StringBuilder();
+        for (int widthValue : barsAndSpaces) {
+            binarySequence.append(widthValue >= ImageProperties.THICK_BAR_WIDTH ? "1" : "0");
+        }
+
+        String trimmedBinary = binarySequence.substring(4, binarySequence.length() - 1);
+
+        StringBuilder evenIndexBits = new StringBuilder();
+        StringBuilder oddIndexBits = new StringBuilder();
+
+        for (int i = 0; i < trimmedBinary.length(); i++) {
+            if (i % 2 == 0) {
+                evenIndexBits.append(trimmedBinary.charAt(i));
+            } else {
+                oddIndexBits.append(trimmedBinary.charAt(i));
+            }
+        }
+
+        String evenDecoded = decodePatternSequence(evenIndexBits.toString());
+        String oddDecoded = decodePatternSequence(oddIndexBits.toString());
+
+        StringBuilder finalBarcode = new StringBuilder();
+        for (int i = 0; i < evenDecoded.length(); i++) {
+            finalBarcode.append(evenDecoded.charAt(i));
+            if (i < oddDecoded.length()) {
+                finalBarcode.append(oddDecoded.charAt(i));
+            }
+        }
+
+        return finalBarcode.toString();
     }
 
-    private int findStartingPoint(BufferedImage image) {
-        int height = image.getHeight();
-        int borderThickness = 8;
-        for (int x = borderThickness; x < image.getWidth(); x++) {
-            for (int y = 0; y < height; y++) {
-                if (image.getRGB(x, y) == Color.BLACK.getRGB()) {
-                    return x;
+    private boolean isBlackColumn(BufferedImage image, int x, int y) {
+        return image.getRGB(x, y) != -1;
+    }
+
+    private String decodePatternSequence(String binarySequence) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < binarySequence.length(); i += 5) {
+            String pattern = binarySequence.substring(i, i + 5);
+
+            for (int j = 0; j < PATTERNS.length; j++) {
+                if (PATTERNS[j].equals(pattern)) {
+                    result.append(j);
+                    break;
                 }
             }
         }
-        return 0;
-    }
 
-    private String readPattern(BufferedImage image, int x, int y, Color expectedColor) {
-        StringBuilder pattern = new StringBuilder();
-        int currentWidth = 0;
-        boolean isPatternContinuing = true;
-
-        while (isPatternContinuing && x < image.getWidth()) {
-            int pixelColor = image.getRGB(x, y);
-            if ((expectedColor == Color.BLACK && pixelColor == Color.BLACK.getRGB()) ||
-                    (expectedColor == Color.WHITE && pixelColor == Color.WHITE.getRGB())) {
-                currentWidth++;
-            } else {
-                isPatternContinuing = false;
-            }
-            x++;
-        }
-
-        if (currentWidth >= 2) {
-            pattern.append('1');
-        } else if (currentWidth == 1) {
-            pattern.append('0');
-        }
-
-        return pattern.toString();
-    }
-
-    private String decodePatternToDigit(String pattern) {
-        for (int i = 0; i < PATTERNS.length; i++) {
-            if (PATTERNS[i].equals(pattern)) {
-                return String.valueOf(i);
-            }
-        }
-        return null;
+        return result.toString();
     }
 }
